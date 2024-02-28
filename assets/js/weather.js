47612d1fd25a1db5d2aff579461c07636172bdbb
aavendan <aavenda@gmail.com>
2024-02-27 20:44:39 -0500

    date, max and min temperature

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,11 +2,21 @@ import { weather_data } from './data.js';
 
 
 let loadDayForecastData = () => {
+    
     let [ guayaquil, ambato, tema ] = weather_data;
-    let { city , ...other} = guayaquil
+    let { city , date, maxtemperature, mintemperature, ...other} = guayaquil
 
     let cityHTML = document.getElementById("city")
     cityHTML.innerHTML  = city;
+
+    let dateHTML = document.getElementById("date")
+    dateHTML.innerHTML  = date;
+
+    let maxtemperatureHTML = document.getElementById("maxtemperature")
+    maxtemperatureHTML.innerHTML  = maxtemperature;
+
+    let mintemperatureHTML = document.getElementById("mintemperature")
+    mintemperatureHTML.innerHTML  = mintemperature;
 }
 
 let loadWeekForecastData = () => {
