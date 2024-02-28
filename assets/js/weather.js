aed34801bc16ecd3b1c970b76de7ec3d33a46975
aavendan <aavenda@gmail.com>
2024-02-27 20:40:40 -0500

    city name

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,12 @@
-
+import { weather_data } from './data.js';
 
 
 let loadDayForecastData = () => {
-	
+    let [ guayaquil, ambato, tema ] = weather_data;
+    let { city , ...other} = guayaquil
+
+    let cityHTML = document.getElementById("city")
+    cityHTML.innerHTML  = city;
 }
 
 let loadWeekForecastData = () => {
